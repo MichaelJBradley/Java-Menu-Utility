411d90807898451e1f3f1a44c2aa95cd3fa8adf5
MichaelJBradley <mbrad94@gmail.com>
2018-02-17 12:37:00 -0800

    Change OptionIterator to implement ListIterator

--- src/com/github/MichaelJBradley/util/Menu/OptionIterator.java
+++ src/com/github/MichaelJBradley/util/Menu/OptionIterator.java
@@ -1,11 +1,12 @@
 package com.github.MichaelJBradley.util.Menu;
 
 import java.util.Collection;
-import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
-public class OptionIterator implements Iterator <Option> {
+public class OptionIterator implements ListIterator <Option> {
 
-	Menu options;
+	List<Option> options;
 	int current;
 	
 	public OptionIterator() {
@@ -13,9 +14,14 @@ public class OptionIterator implements Iterator <Option> {
 	}
 	
 	public OptionIterator(Collection<Option> c) {
-		options = ImmutableMenu(c);
+		setList(c);
 	}
 	
+	public OptionIterator(Menu menu) {
+		setList(menu);
+	}
+	
+	//-- List Iterator --\\
 	@Override
 	public boolean hasNext() {
 		if (current + 1 >= options.size()) {
@@ -30,4 +36,73 @@ public class OptionIterator implements Iterator <Option> {
 		return options.get(current++);
 	}
 
+	@Override
+	public boolean hasPrevious() {
+		if (current - 1 < 0) {
+			return false;
+		}
+		
+		return options.get(current - 1) != null;
+	}
+
+	@Override
+	public Option previous() {
+		return options.get(current--);
+	}
+
+	@Override
+	public int nextIndex() {
+		return current + 1;
+	}
+
+	@Override
+	public int previousIndex() {
+		return current - 1;
+	}
+	
+	/**
+	 * Does nothing.
+	 */
+	@Override
+	public void remove() {
+	}
+
+	/**
+	 * Does nothing.
+	 */
+	@Override
+	public void set(Option e) {
+	}
+
+	/**
+	 * Does nothing.
+	 */
+	@Override
+	public void add(Option e) {
+	}
+
+	//-- Menu --\\
+	/**
+	 * Sets the List reference to an ImmutableMenu object containing the options in c.
+	 * @param c the options to set.
+	 * @return a reference to this object.
+	 */
+	public OptionIterator setList(Collection<Option> c) {
+		options = new ImmutableMenu(c);
+		return this;
+	}
+	
+	/**
+	 * Sets the List reference to menu.
+	 * @param menu the Menu to be set.
+	 * @return a reference to this object.
+	 */
+	public OptionIterator setList(Menu menu) {
+		this.options = menu;
+		return this;
+	}
+	
+	public List<Option> getList() {
+		return options;
+	}
 }
