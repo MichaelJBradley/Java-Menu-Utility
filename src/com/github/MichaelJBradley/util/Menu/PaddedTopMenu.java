d4e5433237479ac8734a1d7d90b06b006f1427f8
Michael Bradley <mbrad94@gmail.com>
2018-07-05 21:48:54 -0700

    Create PaddedTopMenu class

---
+++ src/com/github/MichaelJBradley/util/Menu/PaddedTopMenu.java
@@ -0,0 +1,29 @@
+package com.github.MichaelJBradley.util.Menu;
+
+public class PaddedTopMenu extends PaddedMenu {
+
+	
+	public PaddedTopMenu() {
+		super();
+	}
+	
+	public PaddedTopMenu(Menu menu, int pads) {
+		super(menu, pads);
+	}
+	
+	@Override
+	/**
+	 * Print newlines before the given Menu.
+	 * @return a String representing the menu with {@link #pads} number of newlines added before it.
+	 */
+	public String toString() {
+		StringBuilder ret = new StringBuilder();
+		
+		for (int i = 0; i < pads; i++) {
+			ret.append("\n");
+		}
+		ret.append(menu.toString());
+		
+		return ret.toString();
+	}
+}
